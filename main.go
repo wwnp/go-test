440f89d4da2fc344f0007ee663c09041fbfad6b7
wwnp <iamtodaysad@gmail.com>
2022-06-27 11:44:46 +0300

    int

--- main.go
+++ main.go
@@ -2,63 +2,24 @@ package main
 
 import "fmt"
 
-//------------------------------------------
-// TYPES
-type Age int
-
-type User struct {
-	name   string
-	age    Age
-	sex    string
-	weight int
-	height int
+type Foo interface {
+	getWidth() int
 }
 
-//------------------------------------------
-// METHODS
-func (u User) printUserInfo() {
-	fmt.Println("printUserInfo: ", u.name, u.age, u.sex) //  по value- копируется структура
-} // метод структуры по value; (u User) - ресивер
+type TestStruct struct {
+	width int
+}
 
-func (u User) getName() string {
-	return "getName: " + u.name
-} // метод структуры просто вернуть значение
+func (t TestStruct) getWidth() int {
+	return t.width
+}
 
-func (u *User) setName(newName string) {
-	fmt.Println("setName on:", newName)
-	u.name = newName
-} // метод структуры по pointer(ссылке) то исп та же структура
+func getWidthThroughInterface(f Foo) {
+	fmt.Println(f.getWidth())
+}
 
-func (a Age) isAdult() string {
-	if a >= 18 {
-		return "Can pass"
-	}
-	return "Can't pass "
-} // Метод для Type Age
-
-//------------------------------------------
-// CONSTRUCTOR
-func NewUser(name, sex string, age, weight, height int) User {
-	return User{
-		name:   name,
-		sex:    sex,
-		age:    Age(age),
-		height: height,
-		weight: weight,
-	}
-} // constructor
-
-//------------------------------------------
 func main() {
-	user := NewUser("Pole", "male", 19, 67, 185)
-
-	user.printUserInfo()
-
-	fmt.Println(user.getName())
-
-	user.setName("YANHEN")
-
-	fmt.Println(user.getName())
-
-	fmt.Println(user.age.isAdult())
+	testQ := TestStruct{50}
+	// fmt.Println(testQ.getWidth())
+	getWidthThroughInterface(testQ)
 }
